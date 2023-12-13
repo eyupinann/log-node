13301049d7f523c2033cc51d95fcb1e91f173b6d
Deniz Eyüp İnan <53041708+denizsinan@users.noreply.github.com>
2023-12-13 13:51:55 +0300

    log-node uploaded

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3001;
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.post('/log', (req, res) => {
+    const { timestamp,userAgent,ip } = req.body;
+    console.log('Log request:', req.body);
+
+    const logData = `${timestamp} - IP: ${ip}\nUser-Agent: ${userAgent}\n\n`;
+
+    fs.appendFile('logs.txt', logData, (err) => {
+        if (err) {
+            console.error('Error writing to log file:', err);
+            res.status(500).json({ success: false, message: 'Internal server error' });
+        } else {
+            res.json({ success: true, message: 'Log recorded successfully' });
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
